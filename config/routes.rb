0bc1735007e8d1a34a850ee4ec83093dda52217d
niang makhtar <niang.makhtar@outlook.fr>
2025-03-02 12:22:01 +0100

    rails-stupid-coaching

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,9 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
+
+  root to: "questions#home"
+
   get 'ask', to: 'questions#ask'
 
   # root "posts#index"
